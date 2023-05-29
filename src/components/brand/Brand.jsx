77930a5d0870cbfe83b719daac9eb26d0387a242
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-05-29 17:32:43 +0100

    Brands section

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import './brand.css'; 
+import {google, slack, atlassian, dropbox, shopify} from './imports'
 const Brand = () => {
   return (
-    <div>
-      Brand
+    <div className='gpt3__brand section__padding'>
+      <div>
+        <img src={google} alt="google" />
+        <img src={slack} alt="slack" />
+        <img src={atlassian} alt="atlassian" />
+        <img src={dropbox} alt="dropbox" />
+        <img src={shopify} alt="shopify" />
+      </div>
     </div>
   )
 }
