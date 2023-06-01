766b535ddaff7cddeb6fb401934e606aea199fa7
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-06-01 08:06:01 +0100

    cta section completed

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
     <div className='gpt3__cta'>
       <div className="gpt3__cta-content">
         <p>Request Access to Get Started</p>
-        <h3>Register tofay & start exploring endless possibilities</h3>
+        <h3>Register today & start exploring endless possibilities</h3>
       </div>
       <div className="gpt3__cta-btn">
         <button type='button'>Get Started</button>
