0add79a4c442ce4ace9ec534f5c50afc44807b51
Emmanuel Ogbuji <emmanuelogbuji5@gmail.com>
2023-06-01 07:42:27 +0100

    possibility section

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
 import './possibility.css'; 
+import possibilityImage from '../../assets/possibility.png'
 const Possibility = () => {
   return (
-    <div>
-      Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+      <div className="gpt3__possibility-image">
+        <img src={possibilityImage} alt="possibility" />
+      </div>
+      <div className="gpt3__possibility-content">
+        <h4>Request Access to Get Started</h4>
+        <h1 className='gradient__text'>The Possibilities are beyond your imaginations</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium voluptas tempore ipsum.</p>
+        <h4>Request Access to Get Started</h4>
+      </div>
+      
     </div>
   )
 }
